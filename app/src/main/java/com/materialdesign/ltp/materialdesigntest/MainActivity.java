8aa4760ec6df596b459fd3e6d7497a0e30839de3
ltp <soutrik@esolzmail.com>
2015-07-03 14:48:18 +0530

    Soutrik Ghosh

---
+++ app/src/main/java/com/materialdesign/ltp/materialdesigntest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.materialdesign.ltp.materialdesigntest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ltp on 03/07/15.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
